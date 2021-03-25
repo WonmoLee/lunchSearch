2e6415dc8a399eeedfbdfe28e9421e7931db3ffd
wonmoLee <leewonmo94@gmail.com>
2021-03-26 00:37:54 +0900

    DB연결 테스트 및 완료

---
+++ lunch/src/test/java/com/om/lunch/persistence/DBconnTest.java
@@ -0,0 +1,34 @@
+package com.om.lunch.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class DBconnTest {
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void dbConnTest() {
+		try(Connection con = 
+			DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521:XE",
+					"lunchSVC",
+					"bitc5600")) {
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
